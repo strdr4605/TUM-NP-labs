86bd6b6c4d40f5537c623b124b322ed589b40101
strdr4605 <str.dr4605@gmail.com>
2018-04-07 11:55:59 +0300

    new tests for MetricsAggregator

---
+++ lab2/test/unit/MetricsAggregator.test.js
@@ -0,0 +1,27 @@
+import config from './../../src/config'
+import MetricsAggregator from './../../src/MetricsAggregator'
+
+let metricsAggregator = new MetricsAggregator(config.apiUrl)
+
+test('getKeyAndDevicesPaths Promise return object', () => {
+    expect.assertions(1);
+    return metricsAggregator.getKeyAndDevicesPaths().then(data => {
+        expect(typeof data).toBe('object');
+    });
+});
+
+test('getKeyAndDevicesPaths Promise return object has property key and paths', () => {
+    expect.assertions(2);
+    return metricsAggregator.getKeyAndDevicesPaths().then(data => {
+        expect(data).toHaveProperty('key');
+        expect(data).toHaveProperty('paths');
+    });
+});
+
+test('getKeyAndDevicesPaths Promise return object key type is string, value 42', () => {
+    expect.assertions(2);
+    return metricsAggregator.getKeyAndDevicesPaths().then(data => {
+        expect(typeof data.key).toBe('string');
+        expect(data.key.length).toBe(42);
+    });
+});
